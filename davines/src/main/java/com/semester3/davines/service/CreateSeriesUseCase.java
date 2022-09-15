e748934603ba06263ada6ebf45c635ca5f699a1c
i482834 <v.dyulgeryan@student.fontys.nl>
2022-09-15 10:53:08 +0300

    f

--- davines/src/main/java/com/semester3/davines/service/CreateSeriesUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.semester3.davines.service;
-
-import com.semester3.davines.repository.entity.domain.CreateSeriesRequest;
-import com.semester3.davines.repository.entity.domain.CreateSeriesResponse;
-
-public interface CreateSeriesUseCase {
-
-    CreateSeriesResponse createSeries(CreateSeriesRequest request);
-}
