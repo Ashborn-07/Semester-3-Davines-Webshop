f3a7068fa4b87063402eb35096e8b3b70bb278c9
i482834 <v.dyulgeryan@student.fontys.nl>
2022-10-07 11:26:10 +0300

    Test fix and gitignore update

--- src/main/java/com/semester3/davines/repository/SeriesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.semester3.davines.repository;
-
-import com.semester3.davines.repository.entity.SeriesEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeriesRepository extends JpaRepository<SeriesEntity, Long> {
-
-}
--- src/main/java/com/semester3/davines/repository/entity/enums/UserRoleEnum.java
+++
@@ -1,6 +0,0 @@
-package com.semester3.davines.repository.entity.enums;
-
-public enum UserRoleEnum {
-    USER,
-    ADMIN
-}
